dcfe05a76f9639b95f0c513a8edac0a041e61902
786abubakar0 <46836643+786abubakar0@users.noreply.github.com>
2025-08-28 22:22:13 +0500

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,99 @@
+import "./styles/profile.css"
+import {useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUser } from './UserContext';
+
+import apiClient from './api';
+
+
+
+function ProfilePage(){
+    const navigate = useNavigate();
+    const {user, logout} = useUser();
+    const [userCount, setuserCount] = useState('--');
+
+    useEffect(() => {
+        if (!user) {
+            navigate('/login');
+        }
+    }, [user, navigate]); 
+
+    if (!user) {
+        return <div>Loading Profile ...</div>
+
+    }
+
+    const get_user_data = async()=> {
+        navigate('/userdata');
+    };
+
+    const get_user_count = async () => {
+      try {
+        // Use apiClient to access the protected /profile endpoint
+        const response = await apiClient.get('/getUserCount');
+        setuserCount(response.data.userCount);
+      } catch (error) {
+        await apiClient.post('/logout');
+        console.error('Failed to fetch profile data:', error);
+        alert("Failed to fetch count. ReLogin!");
+        logout();
+        
+      }
+    };
+    
+    
+    const handleSubmit = async(e)=>{
+        e.preventDefault();
+        try{
+            await apiClient.post('/logout');
+            logout();
+            navigate('/login');
+        }
+        catch(error){
+            console.error("There was an error while logout!", error);
+            alert('Log out failed!!');
+
+        }
+    };
+      
+    return(
+        <div className='profile__main-container'>
+        <div className='profile__logout-container'>
+            <div className='profile__heading'>Welcome {user.name}!</div>
+            <div className='profile__logout-form'>
+                <form className='profile__form' onSubmit={handleSubmit}>
+                    <div className='profile__form-item'>
+                        <label htmlFor='username'>Username: {user.username}</label>
+                    </div>
+
+                    <div className='profile__form-item'>
+                        <label htmlFor='email'>Email: {user.email}</label>
+                    </div>
+                    <div className='profile__form-item'>
+                        <label htmlFor='role'>Role: {user.role}</label>
+                    </div>
+                    <div className='profile__form-item'>
+                        <label htmlFor='usercount'>Total Users: {userCount}</label>
+                    </div>
+                    <div className='profile__data-item'>
+                        <button className='profile__count-button' type='button' onClick={get_user_count}>Total Users</button>
+
+                        {user.role === 'admin' && (
+                            // This element is rendered only if role is admin
+                            <button className='profile__data-button' type='button' onClick={get_user_data}>Users Data</button>
+                        )}
+
+                    </div>
+
+                    <div className='profile__button-container'>
+                        <button className='profile__logout-button' id='submit' value='Submit'>Logout</button>
+                    </div>  
+        
+                </form>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default ProfilePage;
---
+++ signup.js
@@ -0,0 +1,102 @@
+import "./styles/signup.css"
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import { Link, useNavigate} from 'react-router-dom';
+import { useUser } from './UserContext';
+
+
+function SignupForm(){
+    const [formData, setFormData] = useState({
+        name: '',
+        username: '',
+        email: '',
+        password: '',
+        role:'admin'
+    });
+    const {user} = useUser();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (user) {
+            navigate('/profile');
+        }
+        }, [user, navigate]); 
+    
+    if (user) {
+        return null; 
+    }
+
+    
+    const handleOptionChange = (e) => {
+        setFormData({...formData, [e.target.name]:e.target.value});
+    };
+    const handleChange = (e)=>{
+        setFormData({...formData, [e.target.name]:e.target.value});
+    };
+
+    const handleSubmit = async(e)=>{
+        e.preventDefault();
+        try{
+            await axios.post("http://localhost:5000/signup", formData);
+            alert('SignUp Successful!');
+            setFormData({name: '', username: '',email: '', password: '', role:'admin'});
+        }
+        catch(error){
+            console.error("There was an error while signup!", error);
+            alert('Sign Up failed!!' + error.response.data);
+
+        }
+    };
+    return(
+    <div className='signup__main-container'>
+        <div className='signup__signup-container'>
+            <div className='signup__heading'>Sign Up</div>
+            <div className='signup__signup-form'>
+                <form className="signup__form" onSubmit={handleSubmit}>
+                    <div className='signup__form-item'>
+                        <label htmlFor="name">Name</label>
+                        <input type='text' id="name" name='name' value={formData.name} onChange={handleChange} required></input>
+                    </div>
+
+                    <div className='signup__form-item'>
+                        <label htmlFor='email'>Email</label>
+                        <input type='email' id='email' name='email' value={formData.email} onChange={handleChange} required></input>
+                    </div>
+
+                    <div className='signup__form-item'>
+                        <label htmlFor='username'>Username</label>
+                        <input type='text' id='username' name='username' value={formData.username} onChange={handleChange} required></input>
+                    </div>
+
+                    <div className='signup__form-item'>
+                        <label htmlFor='password'>Password</label>
+                        <input type='password' id='password' name='password' value={formData.password} onChange={handleChange} required></input>
+                    </div>
+
+                    <div className='signup__form-item'>
+                        <div className="signup__radio">
+                            <div>
+                                <label>Admin</label>
+                                <input type="radio" name="role" onChange={handleOptionChange} value="admin" checked={formData.role === 'admin'}/>
+                            </div>
+
+                            <div>
+                                <label>User</label>
+                                <input type="radio" name="role" onChange={handleOptionChange} value="user" checked={formData.role === 'user'}/>                             
+                            </div>
+                        </div>
+                    </div>
+                    <div className='signup__button-container'>
+                        <Link to='/login'><button className='signup__login-button' type='button' id='login'>Login</button></Link>
+                        <button className='signup__signup-button' type='submit' id='submit' value='Submit'>Sign up</button>
+
+                    </div>
+                    <div className='signup__submit-response'>Submit response</div>                   
+                </form>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default SignupForm;
