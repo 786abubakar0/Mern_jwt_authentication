c4747235f22a4653bbc80814ff94d07f33fddca0
786abubakar0 <46836643+786abubakar0@users.noreply.github.com>
2025-08-29 12:27:37 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -287,7 +287,7 @@ app.post('/logout', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Your server is running!' + process.env.MONGOURI +','+process.env.CORS_ORIGIN);
+  res.send('#Your server is running!' + MongoDbUrl +','+FrontEndOrigin);
 });
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
