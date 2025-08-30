e8c659756d4c7af090e3701f0524cace0a100058
786abubakar0 <46836643+786abubakar0@users.noreply.github.com>
2025-08-30 14:53:13 +0500

    Update login.js

--- Frontend/src/files/login.js
+++ Frontend/src/files/login.js
@@ -1,80 +1,103 @@
-import "./styles/login.css"
-import {useState, useEffect} from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import "./styles/profile.css"
+import {useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useUser } from './UserContext';
-import apiClient from './api'; 
+
+import apiClient from './api';
 
 
-function LoginForm(){
-    const [formData, setFormData] = useState({
-        username: '',
-        password: ''
-    });
-    const SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:5000';
-    const { user, login } = useUser();
+
+function ProfilePage(){
     const navigate = useNavigate();
+    const {user, logout} = useUser();
+    const [userCount, setuserCount] = useState('--');
+    const[isUserLoading, setIsUserLoading] = useState(false);
 
+    useEffect(() => {
+        if (!user) {
+            navigate('/login');
+        }
+    }, [user, navigate]); 
 
-   useEffect(() => {
-          if (user) {
-              navigate('/profile');
-          }
-      }, [user, navigate]); 
-  
-      if (user) {
-          return null; 
-      }
+    if (!user) {
+        return <div>Loading Profile ...</div>
 
+    }
 
-    const handleChange = (e)=>{
-        setFormData({...formData, [e.target.name]:e.target.value});
+    const get_user_data = async()=> {
+        navigate('/userdata');
     };
 
+    const get_user_count = async () => {
+      try {
+        setIsUserLoading(true);
+        // Use apiClient to access the protected /profile endpoint
+        const response = await apiClient.get('/getUserCount');
+        setIsUserLoading(false);
+        setuserCount(response.data.userCount);
+      } catch (error) {
+        setIsUserLoading(false);
+        await apiClient.post('/logout');
+        console.error('Failed to fetch profile data:', error);
+        alert("Failed to fetch count. ReLogin!");
+        logout();
+        
+      }
+    };
+    
+    
     const handleSubmit = async(e)=>{
         e.preventDefault();
         try{
-            const response = await apiClient.post(SERVER_URL + "/login", formData);
-            alert('Login Successful!');
-            const { user, accessToken } = response.data;
-            localStorage.setItem("accessToken", accessToken);
-            login(user);
-            navigate('/profile');
-            setFormData({username: '', password: ''});
+            await apiClient.post('/logout');
+            logout();
+            navigate('/login');
         }
         catch(error){
-            console.error("There was an error while login! ", error);
-            alert('login failed!!' + error.response.data);
+            console.error("There was an error while logout!", error);
+            alert('Log out failed!!');
 
         }
     };
+      
     return(
-    <div className='login__main-container'>
-        <div className='login__login-container'>
-            <div className='login__heading'>Log In</div>
-            <div className='login__login-form'>
-                <form className='login__form' onSubmit={handleSubmit}>
-                    <div className='login__form-item'>
-                        <label htmlFor='username'>Username</label>
-                        <input type='text' id='username' name='username' value={formData.username} onChange={handleChange} required></input>
+        <div className='profile__main-container'>
+        <div className='profile__logout-container'>
+            <div className='profile__heading'>Welcome {user.name}!</div>
+            <div className='profile__logout-form'>
+                <form className='profile__form' onSubmit={handleSubmit}>
+                    <div className='profile__form-item'>
+                        <label htmlFor='username'>Username: {user.username}</label>
                     </div>
 
-                    <div className='login__form-item'>
-                        <label htmlFor='password'>Password</label>
-                        <input type='password' id='password' name='password' value={formData.password} onChange={handleChange} required></input>
+                    <div className='profile__form-item'>
+                        <label htmlFor='email'>Email: {user.email}</label>
                     </div>
-                    <div className='login__button-container'>
-                        <Link to="/signup"><button className='login__signup-button' type='button' id='signup'>Signup</button></Link>
-                        <button className='login__login-button' type='submit' id='submit' value='Submit'>Login</button>
+                    <div className='profile__form-item'>
+                        <label htmlFor='role'>Role: {user.role}</label>
+                    </div>
+                    <div className='profile__form-item'>
+                        <label htmlFor='usercount'>Total Users: {userCount}</label>
+                    </div>
+                    <div className='profile__data-item'>
+                        <button className='profile__count-button' type='button' onClick={get_user_count} disabled={isUserLoading}>{isUserLoading ? 'Wait...' : 'Total Users'}</button>
+
+                        {user.role === 'admin' && (
+                            // This element is rendered only if role is admin
+                            <button className='profile__data-button' type='button' onClick={get_user_data}>Users Data</button>
+                        )}
 
                     </div>
-                    <div className='login__submit-response'>Submit response</div>
-                    
+
+                    <div className='profile__button-container'>
+                        <button className='profile__logout-button' id='submit' value='Submit'>Logout</button>
+                    </div>  
+        
                 </form>
             </div>
-
         </div>
     </div>
     );
 }
 
-export default LoginForm;
+export default ProfilePage;
